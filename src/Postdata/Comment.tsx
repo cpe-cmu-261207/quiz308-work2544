7b36f8c2d0cb64ff9e98fb9f4f95eb34e9d34fac
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2021-09-09 14:45:01 +0700

    done

--- src/Postdata/Comment.tsx
+++ src/Postdata/Comment.tsx
@@ -1,16 +1,16 @@
 import { comments } from "../data/comments"
 import CmTmp from "./CommentTemplate"
 
-const MyComment=()=>{
-const render=()=>{
-    const output:JSX.Element[]=[]
-    const EIEIComment=comments
-    console.log(EIEIComment)
-    output.push(...EIEIComment.map(x=><CmTmp username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies} key={x.username} />))
-    return output
-}    
-return (<div>
-{render()}
-</div>);
+const MyComment = () => {
+    const render = () => {
+        const output: JSX.Element[] = []
+        const EIEIComment = comments
+        console.log(EIEIComment)
+        output.push(...EIEIComment.map(x => <CmTmp username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies} key={x.username} />))
+        return output
+    }
+    return (<div>
+        {render()}
+    </div>);
 }
 export default MyComment;
--- src/Postdata/CommentTemplate.tsx
+++ src/Postdata/CommentTemplate.tsx
@@ -1,32 +1,31 @@
 import { CommentType } from "../data/comments";
 import ReplyFn from "./Reply";
-const CmTmp=({username,userImagePath,commentText,likeNum,replies}:CommentType)=>
-{
-    const renderPeply=()=>{
+const CmTmp = ({ username, userImagePath, commentText, likeNum, replies }: CommentType) => {
+    const renderPeply = () => {
 
         const output: JSX.Element[] = []
-                output.push(...replies.map(x=><ReplyFn username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies} key={x.username} />))  
+        output.push(...replies.map(x => <ReplyFn username={x.username} userImagePath={x.userImagePath} commentText={x.commentText} likeNum={x.likeNum} replies={x.replies} key={x.username} />))
         return output
     }
-return (
-    <div className="flex flex-col">
-<div className="flex p-2 items-start space-x-2">
-  <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
-  <div className="bg-gray-500 rounded-lg p-2">
-    <p className="font-semibold text-white">{username}</p>
-    <p className='text-white'>{commentText}</p>
-    {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
-    {
-    likeNum===0?null:    
-    <div className='flex items-center'>
-      <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-      <p className='text-gray-300'>{likeNum}</p>
-    </div>
-    }
-  </div>
-</div>
-{renderPeply()}
-</div>
-);
+    return (
+        <div className="flex flex-col">
+            <div className="flex p-2 items-start space-x-2">
+                <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
+                <div className="bg-gray-500 rounded-lg p-2">
+                    <p className="font-semibold text-white">{username}</p>
+                    <p className='text-white'>{commentText}</p>
+                    {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+                    {
+                        likeNum === 0 ? null :
+                            <div className='flex items-center'>
+                                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                                <p className='text-gray-300'>{likeNum}</p>
+                            </div>
+                    }
+                </div>
+            </div>
+            {renderPeply()}
+        </div>
+    );
 }
 export default CmTmp
